e34b7a47ff652630559318c471b3ed29a720c23e
Por <por@ubakong.com>
2020-01-13 20:02:30 +0700

    updater styled

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function createWindow() {
   Menu.setApplicationMenu(mainMenu)
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
