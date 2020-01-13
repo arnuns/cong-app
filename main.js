c09190421100db928a98aaf2ac0b3af8f2e74d5c
Por <por@ubakong.com>
2020-01-13 21:11:33 +0700

    check for update and notify

--- main.js
+++ main.js
@@ -76,7 +76,10 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  autoUpdater.checkForUpdatesAndNotify()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
