a8f668857136bdb8d28fd764af62edd5d9ae2c85
Por <por@ubakong.com>
2020-02-20 11:20:13 +0700

    modified release date

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,5 +13,5 @@ export const environment = {
   companyEmail: 'office@gmgroup.co.th',
   companyWebsite: 'www.gmsecurity.co.th',
   appVersion: require('../../package.json').version,
-  releaseDate: '06/02/2020'
+  releaseDate: '20/02/2020'
 };
