a91dbf213bd0628c70f14342637fc4bb93de3b9c
Por <por@ubakong.com>
2020-03-10 11:24:07 +0700

    change api url for production env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://testapi.congapps.com',
+  apiUrl: 'https://api.congapps.com',
   // apiUrl: 'http://localhost:5000',
   apiVersion: '1.0',
   // basePath: '../dist/assets',
