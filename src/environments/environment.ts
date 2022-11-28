49e085dce60fcd0176d022a12d451a847fdd6afa
Por <por@ubakong.com>
2022-11-28 11:55:13 +0700

    update signature space and release date

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,6 @@ export const environment = {
   companyEmail: 'info@gmgroup.co.th',
   companyWebsite: 'www.gmsecurity.co.th',
   appVersion: require('../../package.json').version,
-  releaseDate: '24/11/2022',
+  releaseDate: '28/11/2022',
   storageBucketUrl: 'https://storage.googleapis.com/cong-app.appspot.com'
 };
