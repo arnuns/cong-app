7c2284f7279034bd5e7fce25b6510d75db9cc3dc
Por <por@ubakong.com>
2023-02-02 18:44:15 +0700

    update wrong word spell on application form

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,6 @@ export const environment = {
   companyEmail: 'info@gmgroup.co.th',
   companyWebsite: 'www.gmsecurity.co.th',
   appVersion: require('../../package.json').version,
-  releaseDate: '17/01/2023',
+  releaseDate: '02/02/2023',
   storageBucketUrl: 'https://storage.googleapis.com/cong-app.appspot.com'
 };
