2ccb339445c917b0fe5c5235ab13b50d6a018c55
Por <por@ubakong.com>
2023-03-08 11:50:32 +0700

    update release date

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,6 @@ export const environment = {
   companyEmail: 'info@gmgroup.co.th',
   companyWebsite: 'www.gmsecurity.co.th',
   appVersion: require('../../package.json').version,
-  releaseDate: '07/03/2023',
+  releaseDate: '08/03/2023',
   storageBucketUrl: 'https://storage.googleapis.com/cong-app.appspot.com'
 };
